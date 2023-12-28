f41323b3577fb7f63e31df8fbfc5378813d005a6
8467042733 <rupeshpal25112004@gmail.com>
2023-12-28 17:22:42 +0530

    add some code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+  fs.appendFile(fileName,fileContent, function(err) {
+    if(err) throw err;
+    console.log("File Updated");
+});
 };
-
+updateFile(fileName, "Newton School, An online learning platform");
 module.exports = updateFile;
